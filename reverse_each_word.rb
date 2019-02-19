73c806dd4fb2ecbb6a540391d85cfc9e50157fc1
Nataliya R <n.rudenko@gmail.com>
2019-02-19 00:27:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++
@@ -1,9 +0,0 @@
-def reverse_each_word(phrase)
-  array = phrase"Hello there, and how are you?"
-  phrase.split(" ")
-  returning = []
-  array.each do |word| 
-    returning << word.reverse
-  end
-  returning.join(" ")
-end
